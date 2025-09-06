8575979fa918eb5ba71cb60ea1b472a920a95585
Alireza Bohlol <dev.alirezabohlool@gmail.com>
2025-09-06 12:21:21 +0330

    Update layout title and correct spelling in home component; replace header with image and adjust layout styles.

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
 
   const links = [
     { label: "خانه", href: "/" },
-    { label: "داشبور", href: "/" },
+    { label: "داشبورد", href: "/" },
     { label: "تماس با ما", href: "/contact" },
     { label: "خدمات", href: "#" },
   ];
@@ -85,20 +85,11 @@ export default function Home() {
       </nav>
 
       {/* Landing Section */}
-      <main className="min-h-screen flex flex-col items-center justify-center px-6 py-12 bg-[#f8f9fb] text-[var(--foreground)] font-Morabba pt-32">
+      <main className="min-h-screen flex flex-col items-center justify- px-6  text-[var(--foreground)] font-Morabba mt-40">
         {/* Header */}
-        <h1 className="text-3xl md:text-5xl font-extrabold text-center text-[var(--main-color)] mb-4 leading-tight text-balance max-w-4xl mx-auto">
-          سامانه آنلاین و هوشمند<br />پت خانگی، ایران راد
-        </h1>
+        <Image src="/images/final.png" alt="header" width={600} height={600} />
 
-        {/* Description */}
-        <div className=" text-gray-600 text-base md:text-xl lg:max-w-2xl max-w-4xl mb-8 space-y-2 text-center font-extrabold leading-relaxed ">
-          <p>
-            مرجع جامع ثبت اطلاعات، مدیریت، سلامت و دریافت مشاوره تخصصی غذایی و درمانی. هم اکنون وارد حساب کاربری خود شوید یا ثبت نام کنید.
-          </p>
-        </div>
-
-        {/* CTA Buttons */}
+        {/* CTA Buttons */} 
         {isLoggedIn ? (
           <Link href="/dashboard">
             <button className="flex cursor-pointer items-center gap-2 bg-[var(--main-color)] hover:bg-[var(--main-color-dark)] text-white px-6 py-3 rounded-xl text-base font-semibold shadow-md transition-all">
